c2e7de8a1880e24af62b74cd4c11184d001f4a37
kennygeiler <kennethlgeiler@gmail.com>
2015-10-13 12:09:28 -0500

    update reffer table

--- metajob/db/migrate/20151013165205_create_reffers.rb
+++ metajob/db/migrate/20151013165205_create_reffers.rb
@@ -1,7 +1,8 @@
 class CreateReffers < ActiveRecord::Migration
   def change
     create_table :reffers do |t|
-
+      t.string :invite_code
+      t.string :ref_code
       t.timestamps null: false
     end
   end
