1bf2c7f42b9abc6f1cccced6d2d1b5f1f05bdf39
kennygeiler <kennethlgeiler@gmail.com>
2015-10-13 12:23:37 -0500

    update schema

--- metajob/db/schema.rb
+++ metajob/db/schema.rb
@@ -11,11 +11,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151013165205) do
+ActiveRecord::Schema.define(version: 20151013172211) do
+
+  create_table "applications", force: :cascade do |t|
+    t.string   "full_name"
+    t.string   "email"
+    t.string   "github_link"
+    t.string   "linkedin_link"
+    t.string   "resume"
+    t.string   "ref_code"
+    t.integer  "job_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "companies", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.string   "position"
+    t.string   "ref_fee"
+    t.text     "description"
+    t.integer  "company_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "reffers", force: :cascade do |t|
