636e33519eb903bfaed23d85b08a91a942875c37
kennygeiler <kennethlgeiler@gmail.com>
2015-10-13 12:10:40 -0500

    update company migration

--- metajob/db/migrate/20151013165138_create_companies.rb
+++ metajob/db/migrate/20151013165138_create_companies.rb
@@ -1,7 +1,9 @@
 class CreateCompanies < ActiveRecord::Migration
   def change
     create_table :companies do |t|
-
+      t.string :name
+      t.string :city
+      t.text :description
       t.timestamps null: false
     end
   end
