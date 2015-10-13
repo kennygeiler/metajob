211340a2f5a6c55ee8cbc6ece8feb9a602f0b651
kennygeiler <kennethlgeiler@gmail.com>
2015-10-13 11:50:54 -0500

    set up polymorphic assocations

--- metajob/app/models/user.rb
+++ metajob/app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-  rolify
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
   belongs_to :meta, polymorphic: true
