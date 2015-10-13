68c47abfbfcb191363d09f41b97296ccc74e9c70
kennygeiler <kennethlgeiler@gmail.com>
2015-10-13 18:52:56 -0500

    update

--- metajob/app/models/user.rb
+++ metajob/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
-  belongs_to :meta, polymorphic: true
+
 
 end
