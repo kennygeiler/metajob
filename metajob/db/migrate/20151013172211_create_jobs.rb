93a85289251fc6efccf66cc5fde38543a523b09b
kennygeiler <kennethlgeiler@gmail.com>
2015-10-13 12:22:56 -0500

    add columns to jobs table

--- metajob/db/migrate/20151013172211_create_jobs.rb
+++ metajob/db/migrate/20151013172211_create_jobs.rb
@@ -1,7 +1,10 @@
 class CreateJobs < ActiveRecord::Migration
   def change
     create_table :jobs do |t|
-
+      t.string :position
+      t.string :ref_fee
+      t.text :description
+      t.integer :company_id
       t.timestamps null: false
     end
   end
