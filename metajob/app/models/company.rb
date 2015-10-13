1f0ae918c7f6645777b2e0ddbaa0a5831adefa83
kennygeiler <kennethlgeiler@gmail.com>
2015-10-13 12:05:14 -0500

    update schema

--- metajob/app/models/company.rb
+++ metajob/app/models/company.rb
@@ -1,3 +1,4 @@
 class Company < ActiveRecord::Base
   has_one :user, as: :meta, dependent: :destroy
+  accepts_nested_attributes_for :user
 end
--- metajob/db/schema.rb
+++ metajob/db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151013164757) do
+ActiveRecord::Schema.define(version: 20151013165205) do
+
+  create_table "companies", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reffers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -26,9 +36,12 @@ ActiveRecord::Schema.define(version: 20151013164757) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "meta_id"
+    t.string   "meta_type"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["meta_id", "meta_type"], name: "index_users_on_meta_id_and_meta_type"
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
