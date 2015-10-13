a28ff785a80ff12dae710793ecac8fa4c2ba4e6e
kennygeiler <kennethlgeiler@gmail.com>
2015-10-13 12:23:20 -0500

    add columns to application table

--- metajob/db/migrate/20151013172158_create_applications.rb
+++ metajob/db/migrate/20151013172158_create_applications.rb
@@ -1,7 +1,13 @@
 class CreateApplications < ActiveRecord::Migration
   def change
     create_table :applications do |t|
-
+      t.string :full_name
+      t.string :email
+      t.string :github_link
+      t.string :linkedin_link
+      t.string :resume
+      t.string :ref_code
+      t.integer :job_id
       t.timestamps null: false
     end
   end
