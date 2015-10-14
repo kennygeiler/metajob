8cb4f7b10a738ab054841dfd04b4f386cc61e51d
kennygeiler <kennethlgeiler@gmail.com>
2015-10-13 22:30:45 -0500

    modify jobs params

--- metajob/app/controllers/jobs_controller.rb
+++ metajob/app/controllers/jobs_controller.rb
@@ -54,13 +54,14 @@ class JobsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_job
-      @job = Job.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_job
+    @job = Job.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def job_params
-      params[:job]
-    end
+
+  def job_params
+    params.require(:job).permit(:position, :ref_fee, :description, :company_id)
+  end
+
 end
