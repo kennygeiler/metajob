13cf3d335d31bebdf449acf15b7a0e81b6b9388c
kennygeiler <kennethlgeiler@gmail.com>
2015-10-13 22:46:37 -0500

    generate application files

--- metajob/config/routes.rb
+++ metajob/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :applications
   resources :jobs
   devise_for :users, controllers: {registrations: "users/registrations"}
   devise_for :companies, controllers: {registrations: "companies/registrations"}
