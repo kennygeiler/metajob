30373ff6dcafcab69ad27babd95ec23e1b79ce28
kennygeiler <kennethlgeiler@gmail.com>
2015-10-18 18:43:12 -0500

    generate user code in background, FUCK YEAH

--- metajob/app/controllers/users/registrations_controller.rb
+++ metajob/app/controllers/users/registrations_controller.rb
@@ -1,15 +1,11 @@
 # app/controllers/users/registrations_controller.rb
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_filter :configure_permitted_parameters, :generate_code
+  before_filter :configure_permitted_parameters
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up).push(:ref_code, :invite_code)
-  end
-
-  def generate_code
-    self.ref_code = rand(36**8).to_s(36);
   end
 
 end
--- metajob/app/models/user.rb
+++ metajob/app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
+  before_create :generate_code
+
+  def generate_code
+    self.ref_code = rand(36**8).to_s(36);
+  end
+
 end
