b08e3059a609d683ffc32b8de654c24cf8f002b2
kennygeiler <kennethlgeiler@gmail.com>
2015-10-18 16:02:42 -0500

    change migration

--- metajob/db/migrate/20151018205633_add_pic_paperclip.rb
+++ metajob/db/migrate/20151018205633_add_pic_paperclip.rb
@@ -1,15 +1,15 @@
 class AddPicPaperclip < ActiveRecord::Migration
   def self.up
-    add_column :applications, :pic_file_name,    :string
-    add_column :applications, :pic_content_type, :string
-    add_column :applications, :pic_file_size,    :integer
-    add_column :applications, :pic_updated_at,   :datetime
+    add_column :applications, :resume_file_name,    :string
+    add_column :applications, :resume_content_type, :string
+    add_column :applications, :resume_file_size,    :integer
+    add_column :applications, :resume_updated_at,   :datetime
   end
 
   def self.down
-    remove_column :applications, :pic_file_name
-    remove_column :applications, :pic_content_type
-    remove_column :applications, :pic_file_size
-    remove_column :applications, :pic_updated_at
+    remove_column :applications, :resume_file_name
+    remove_column :applications, :resume_content_type
+    remove_column :applications, :resume_file_size
+    remove_column :applications, :resume_updated_at
   end
 end
