7784d2c8eee95a0c67e516a73ad6b6854127a6af
kennygeiler <kennethlgeiler@gmail.com>
2015-10-18 18:37:58 -0500

    maybe generated codes

--- metajob/app/controllers/users/registrations_controller.rb
+++ metajob/app/controllers/users/registrations_controller.rb
@@ -1,10 +1,15 @@
 # app/controllers/users/registrations_controller.rb
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_filter :configure_permitted_parameters
+  before_filter :configure_permitted_parameters, :generate_code
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up).push(:ref_code, :invite_code)
   end
+
+  def generate_code
+    self.ref_code = rand(36**8).to_s(36);
+  end
+
 end
