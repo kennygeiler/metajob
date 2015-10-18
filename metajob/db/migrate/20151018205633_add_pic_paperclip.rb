637da99584f1c247e7919902f9b7e20640091239
kennygeiler <kennethlgeiler@gmail.com>
2015-10-18 15:59:19 -0500

    add info to second table model

--- metajob/db/migrate/20151018205633_add_pic_paperclip.rb
+++ metajob/db/migrate/20151018205633_add_pic_paperclip.rb
@@ -1,4 +1,15 @@
 class AddPicPaperclip < ActiveRecord::Migration
-  def change
+  def self.up
+    add_column :applications, :pic_file_name,    :string
+    add_column :applications, :pic_content_type, :string
+    add_column :applications, :pic_file_size,    :integer
+    add_column :applications, :pic_updated_at,   :datetime
+  end
+
+  def self.down
+    remove_column :applications, :pic_file_name
+    remove_column :applications, :pic_content_type
+    remove_column :applications, :pic_file_size
+    remove_column :applications, :pic_updated_at
   end
 end
