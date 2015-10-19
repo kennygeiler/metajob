11247d9fa75156c9834c57f61d90d63afc06462c
kennygeiler <kennethlgeiler@gmail.com>
2015-10-18 20:43:16 -0500

    Add company controller and index and show view

--- metajob/config/routes.rb
+++ metajob/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :applications
   resources :jobs
+  resources :companies
   devise_for :users, controllers: {registrations: "users/registrations"}
   devise_for :companies, controllers: {registrations: "companies/registrations"}
   root 'jobs#index'
