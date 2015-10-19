a749c22dd970d586461bc5fe723afa06d28d6cb7
kennygeiler <kennethlgeiler@gmail.com>
2015-10-18 20:35:47 -0500

    set root page to jobs index

--- metajob/config/routes.rb
+++ metajob/config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :jobs
   devise_for :users, controllers: {registrations: "users/registrations"}
   devise_for :companies, controllers: {registrations: "companies/registrations"}
+  root 'jobs#index'
 
 end
