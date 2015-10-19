c58e785b86570444de9bb09f7cc8bb6f014ceba9
kennygeiler <kennethlgeiler@gmail.com>
2015-10-19 12:56:36 -0500

    running into problems here

--- metajob/app/models/application.rb
+++ metajob/app/models/application.rb
@@ -1,4 +1,5 @@
 class Application < ActiveRecord::Base
   belongs_to :job
-  has_attached_file :attach
+  has_attached_file :document
+  validates_attachment :document, :content_type => { :content_type => %w(application/pdf) }
 end
