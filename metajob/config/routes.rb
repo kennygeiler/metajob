dbfe76e86009fc3c6fd08b8eaca0b61e37588af5
kennygeiler <kennethlgeiler@gmail.com>
2015-10-19 13:56:53 -0500

    add company routes and view template

--- metajob/config/routes.rb
+++ metajob/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :applications
   resources :jobs
 
+  resources :companies, only: [:index, :show]
   devise_for :users, controllers: {registrations: "users/registrations"}
   devise_for :companies, controllers: {registrations: "companies/registrations"}
   root 'jobs#index'
