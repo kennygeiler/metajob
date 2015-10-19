f81e6ffad6524ddec67d57bfb00ec8f7a5cf7a34
kennygeiler <kennethlgeiler@gmail.com>
2015-10-19 13:05:35 -0500

    add uploader validation

--- metajob/app/models/application.rb
+++ metajob/app/models/application.rb
@@ -1,5 +1,4 @@
 class Application < ActiveRecord::Base
   belongs_to :job
-  has_attached_file :document
-  validates_attachment :document, :content_type => { :content_type => %w(application/pdf) }
+  mount_uploader :attachment, AttachmentUploader
 end
