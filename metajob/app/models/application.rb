be41ee243786d23db0e640a8b7d05e2ae581d0f5
kennygeiler <kennethlgeiler@gmail.com>
2015-10-19 13:19:21 -0500

    fix naming

--- metajob/app/models/application.rb
+++ metajob/app/models/application.rb
@@ -1,4 +1,4 @@
 class Application < ActiveRecord::Base
   belongs_to :job
-  mount_uploader :resume, AttachmentUploader
+  mount_uploader :resume, ResumeUploader
 end
--- metajob/app/uploaders/resume_uploader.rb
+++ metajob/app/uploaders/resume_uploader.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-class AttachmentUploader < CarrierWave::Uploader::Base
+class ResumeUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
@@ -41,10 +41,6 @@ class AttachmentUploader < CarrierWave::Uploader::Base
   # def extension_white_list
   #   %w(jpg jpeg gif png)
   # end
-
-  def extension_white_list
-    %w(pdf doc htm html docx)
-  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
