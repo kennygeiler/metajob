8029a6567185e4f2cd741943c41deb7062e41f22
kennygeiler <kennethlgeiler@gmail.com>
2015-10-19 14:13:29 -0500

    running into problems w devise controllers and normal controllers

--- metajob/config/routes.rb
+++ metajob/config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   resources :applications
   resources :jobs
-
-  resources :companies, only: [:index, :show]
   devise_for :users, controllers: {registrations: "users/registrations"}
   devise_for :companies, controllers: {registrations: "companies/registrations"}
   root 'jobs#index'
