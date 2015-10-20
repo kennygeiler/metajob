ebb61f403fa8cc45b6f9cbd176dba50c0ca8010d
kennygeiler <kennethlgeiler@gmail.com>
2015-10-20 17:40:27 -0500

    delete unused methods

--- metajob/app/controllers/applications_controller.rb
+++ metajob/app/controllers/applications_controller.rb
@@ -1,24 +1,9 @@
 class ApplicationsController < ApplicationController
-  before_action :set_application, only: [:show, :edit, :update, :destroy]
 
-
-
-  # GET /applications/1
-  # GET /applications/1.json
-  def show
-  end
-
-  # GET /applications/new
   def new
     @application = Application.new
   end
 
-  # GET /applications/1/edit
-  def edit
-  end
-
-  # POST /applications
-  # POST /applications.json
   def create
     @application = Application.new(application_params)
 
@@ -30,30 +15,6 @@ class ApplicationsController < ApplicationController
         format.html { render :new }
         format.json { render json: @application.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /applications/1
-  # PATCH/PUT /applications/1.json
-  def update
-    respond_to do |format|
-      if @application.update(application_params)
-        format.html { redirect_to @application, notice: 'Application was successfully updated.' }
-        format.json { render :show, status: :ok, location: @application }
-      else
-        format.html { render :edit }
-        format.json { render json: @application.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /applications/1
-  # DELETE /applications/1.json
-  def destroy
-    @application.destroy
-    respond_to do |format|
-      format.html { redirect_to applications_url, notice: 'Application was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
