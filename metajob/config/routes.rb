09a6bc74b6262defc2499d57bd97366b18d8ecfb
kennygeiler <kennethlgeiler@gmail.com>
2015-10-20 22:37:28 -0500

    config routes

--- metajob/config/routes.rb
+++ metajob/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :jobs
   devise_for :users, controllers: {registrations: "users/registrations"}
   devise_for :companies, controllers: {registrations: "companies/registrations"}
-  root 'jobs#index'
+  get "/pages/:page" => "pages#show"
+  root "pages#show"
 
 end
