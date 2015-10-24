794337e7a963595b79cf30eacbe5ea132729628f
kennygeiler <kennethlgeiler@gmail.com>
2015-10-24 15:01:58 -0500

    add errror routes

--- metajob/config/routes.rb
+++ metajob/config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   devise_for :companies, controllers: {registrations: "companies/registrations"}
   get "/pages/:page" => "pages#show"
   root "pages#show", page: "home"
+  get "/404", :to => "errors#not_found"
+  get "/422", :to => "errors#unacceptable"
+  get "/500", :to => "errors#internal_error"
+
 
 end
